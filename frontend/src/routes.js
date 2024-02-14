c9ddd612338d5aa758442384caa9cf2e332a964a
Will Sams <will@samswebs.com>
2024-02-14 09:40:17 -0500

    add limiter to authentication routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,12 +3,18 @@ import reservation_controller from './controllers/reservation.js';
 import { protectRoute } from './middleware/authentication.js';
 import { authSession } from './middleware/session.js';
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per window
+  message: "Too many requests from this IP, please try again later"
+});
+
 const routes = app => {
   [
     app.get(['/', '/home'], authSession, home_controller.index),
     app.get('/about', home_controller.about),
-    app.get('/login', [authSession, protectRoute], home_controller.login),
-    app.get('/logout', [authSession, protectRoute], home_controller.logout),
+    app.get('/login', [authSession, protectRoute, limiter], home_controller.login),
+    app.get('/logout', [authSession, protectRoute, limiter], home_controller.logout),
 
     app.get('/reservation/new', authSession, reservation_controller.newReservation),
     app.post('/reservation/create', authSession, reservation_controller.createReservation),
